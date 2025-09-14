aa993b453c158eaa017f0e198a6900513aa0ec0e
devsujalpatel <sujal7455@gmail.com>
2025-09-14 21:27:57 +0530

    basic get route in go

--- main.go
+++ main.go
@@ -11,5 +11,9 @@ func main() {
 	fmt.Println("Hello World!")
 	app := fiber.New()
 
+	app.Get("/", func (c *fiber.Ctx) error  {
+		return c.Status(200).JSON(fiber.Map{"message": "Hello World!"})
+	})
+
 	log.Fatal(app.Listen(":4000"))
 }
